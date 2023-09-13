211cf35a2d79e854205a40c26a06f87bc688fd15
MudassirQuraishi <mudassir.quraishi14@outlook.com>
2023-09-13 13:58:35 +0530

    server file updated

--- app.js
+++ app.js
@@ -23,10 +23,6 @@ const expenseRoutes = require("./routes/expenseRoutes");
 const purchaseRoutes = require("./routes/purchaseRoutes");
 const premiumRoutes = require("./routes/premiumRoutes");
 const resetRoutes = require("./routes/passwordResetRoutes");
-const accessLogStream = fs.createWriteStream(
-  path.join(__dirname, "accesslog.txt"),
-  { flags: "a" }
-);
 
 //table relations
 User.hasMany(Expense);
@@ -43,18 +39,9 @@ ReportFiles.belongsTo(User);
 
 const app = express();
 
-// app.use(function (req, res, next) {
-//   res.setHeader(
-//     "Content-Security-Policy-Report-Only",
-//     "default-src 'self'; font-src 'self'; connect-src 'self' http://13.211.123.38:3000/expense/get-expense; img-src 'self'; script-src 'self' https://cdnjs.cloudflare.com/ajax/libs/axios/1.5.0/axios.min.js https://checkout.razorpay.com/v1/checkout.js; style-src 'self'; frame-src 'self' https://checkout.razorpay.com/v1/checkout.js  https://api.razorpay.com"
-//   );
-//   next();
-// });
-
 app.use(cors());
 app.use(bodyParser.json());
 app.use(compression());
-app.use(morgan("combined", { stream: accessLogStream }));
 app.use(express.static(path.join(__dirname, "public")));
 
 //middleware
