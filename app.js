b30a4ff8330ff72b632f63793fdd0b2536c67ff5
MudassirQuraishi <mudassir.quraishi14@outlook.com>
2023-09-13 08:16:12 +0530

    helmet removed

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ ReportFiles.belongsTo(User);
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
-app.use(helmet());
+// app.use(helmet());
 app.use(compression());
 app.use(morgan("combined", { stream: accessLogStream }));
 app.use(express.static(path.join(__dirname, "public")));
